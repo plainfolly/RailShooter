6a9a890b1f7ccd00d88b6a3a48d1383c045b942b
Emilia Baron <emilia.baron@outlook.de>
2020-07-26 22:57:19 +0200

    Set up loading Screen + DontDestoyOnLoad to continue music throughout the scenes

---
+++ Assets/Prefabs/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    
+    private void Awake ()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        Invoke("LoadFirstScene", 5f);
+    }
+
+    
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
