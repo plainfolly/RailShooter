64f501f3f6f177467679505b273158de5a9311f5
Ray Miecznikowski <raymondmiecz@gmail.com>
2020-07-30 23:22:09 +0200

    added horizontal movement, not working tho - Emilia

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        transform.localPosition = new Vector3(rawNewXPos, transform.localPosition.y, transform.localPosition.z); 
+    }
+}
